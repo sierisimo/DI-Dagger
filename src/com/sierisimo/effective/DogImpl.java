341ba2cac572a858b8e4f710d9484e1966ec4bdd
Sinuhe Jaime <sinuhe_jaime@epam.com>
2017-07-19 18:23:00 -0500

    Deleted not important/relevant files

--- src/com/sierisimo/effective/DogImpl.java
+++
@@ -1,7 +0,0 @@
-package com.sierisimo.effective;
-
-/**
- * Created by Sinuhe_Jaime on 5/30/2017.
- */
-public class DogImpl extends AbstractDog implements IDog {
-}
--- src/com/sierisimo/playground/Sier.java
+++
@@ -1,7 +0,0 @@
-package com.sierisimo.playground;
-
-/**
- * Created by Sinuhe_Jaime on 5/22/2017.
- */
-public class Sier {
-}
